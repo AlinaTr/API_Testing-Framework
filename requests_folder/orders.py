bdc90620f260ecd172652e5a98b11cf4e2c41dcc
AlinaTr <atrandafir78@gmail.com>
2023-06-12 18:38:05 +0300

    added more tests

--- requests_folder/orders.py
+++ requests_folder/orders.py
@@ -1,33 +1,26 @@
 import requests
 
 def submit_orders(book_id, customer_name, token):
-
     json_data = {
 
             "bookId": book_id,
             "customerName": customer_name
         }
-
     headers_data = {
         'Authorization': f'Bearer: {token}'
     }
-
     response = requests.post(
         url='https://simple-books-api.glitch.me/orders',
         json=json_data,
         headers=headers_data,
 
     )
-
     return response
 
 def get_all_orders(token):
-
-
     headers_data={
         'Authorization':f'Bearer:{token}'
     }
-
     response = requests.get(
         url='https://simple-books-api.glitch.me/orders',
         headers=headers_data
--- tests/tests_orders.py
+++ tests/tests_orders.py
@@ -1,9 +1,9 @@
-from requests_folder.orders import submit_orders,get_all_orders, get_an_order, update_an_order, delete_an_order
+from requests_folder.orders import submit_orders, get_all_orders, get_an_order, update_an_order, delete_an_order
 
 class TestSubmitOrder:
 
     token = "c222e0152efecdb053b82b02ff61bffacdcc6ed4f97d7b7bf6ff6a1c148cad72"
-# acest test pica
+# aceste teste pica
     def test_when_all_data_is_valid(self):
         response = submit_orders(5, 'AlinaTrandafir500',self.token)
         assert response.status_code == 201
